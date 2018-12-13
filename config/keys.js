66251cf0a45627183bf3459cec0178e5212454ea
Souhail <mr.souid@live.fr>
2018-12-13 15:36:30 +0100

    secure key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,10 @@
-module.exports = {
-	mongoURI: process.env.DB_DATABASE,
-	secretOrKey: process.env.DB_secretOrKey
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod')
+} else {
+	module.exports = require('./keys_dev')
 }
+
+// module.exports = {
+// 	mongoURI: process.env.DB_DATABASE,
+// 	secretOrKey: process.env.DB_secretOrKey
+// }
